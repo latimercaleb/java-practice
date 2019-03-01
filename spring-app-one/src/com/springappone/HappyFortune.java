512fbc28e6e20f4cd53f931646a5b19da2dcbdde
latimercaleb <nohitokaze@gmail.com>
2019-03-01 00:56:01 -0500

    Excercise 2 app extended

--- spring-app-one/src/com/springappone/HappyFortune.java
+++ spring-app-one/src/com/springappone/HappyFortune.java
@@ -1,12 +1,15 @@
 package com.springappone;
 
+import java.util.Random;
+
 public class HappyFortune implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		// Return good or bad fortune based on rand num
-		// Fix this up a bit for practice activity 2
-		return "Today is your lucky day";
+		Random rand = new Random();
+		int randNum = rand.nextInt(3);
+		String [] randFortune = {"Today isn't your day","Luck embodied, fortune favors you","Today is useless, go back to bed"};
+		return  randFortune[randNum];
 	}
 	
 }

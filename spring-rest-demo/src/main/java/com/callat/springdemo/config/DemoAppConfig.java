1a894b04269f59dd8ec06d2291bc77ac670b01f5
latimercaleb <nohitokaze@gmail.com>
2019-08-14 00:26:41 -0400

    Adding initialization and config

---
+++ spring-rest-demo/src/main/java/com/callat/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.callat.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.callat.springdemo")
+public class DemoAppConfig {
+
+}
